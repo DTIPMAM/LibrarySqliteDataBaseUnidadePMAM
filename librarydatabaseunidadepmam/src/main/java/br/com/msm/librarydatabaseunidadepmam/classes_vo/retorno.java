f68c5b4f0809add4abf4e9b0f9a7cd2c24db6de8
maycon medeiros <maycon1038@gmail.com>
2025-09-15 12:48:40 -0400

    [REFACTOR] Remove Lombok e adiciona getters/setters manualmente
    - Remove a dependência do Lombok do arquivo `build.gradle` do módulo `librarydatabaseunidadepmam`.
    - Remove a anotação `@Data` das classes `dados` e `retorno`.
    - Adiciona manualmente os métodos getters e setters para todos os atributos das classes `dados` e `retorno`.

--- librarydatabaseunidadepmam/src/main/java/br/com/msm/librarydatabaseunidadepmam/classes_vo/retorno.java
+++ librarydatabaseunidadepmam/src/main/java/br/com/msm/librarydatabaseunidadepmam/classes_vo/retorno.java
@@ -1,11 +1,36 @@
 package br.com.msm.librarydatabaseunidadepmam.classes_vo;
 
-import lombok.Data;
 
-@Data
+
+
 public class retorno {
 
     private String retorno;
+
+    public String getRetorno() {
+        return retorno;
+    }
+
+    public void setRetorno(String retorno) {
+        this.retorno = retorno;
+    }
+
+    public String getItem_count() {
+        return item_count;
+    }
+
+    public void setItem_count(String item_count) {
+        this.item_count = item_count;
+    }
+
+    public int getTotalPager() {
+        return totalPager;
+    }
+
+    public void setTotalPager(int totalPager) {
+        this.totalPager = totalPager;
+    }
+
     private String item_count;
     private int totalPager;
 }
